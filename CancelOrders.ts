fbafb62f7021071224e9b4c6dd4de81d863361fa
Elihai Vanunu <elihaidv@gmail.com>
2022-10-01 21:13:33 +0300

    pipe

--- CancelOrders.ts
+++ CancelOrders.ts
@@ -1,7 +1,7 @@
 import { Bot } from "./Models";
 
 async function cancelOrders(bot: Bot, pair?) {
-    const PAIR = pair == 0 ? (bot.coin1 + bot.coin2) : pair
+    const PAIR = pair ? pair : (bot.coin1 + bot.coin2) 
     if (bot.binance && bot.binance!.orders[PAIR]) {
         const openOrders = bot.binance!.orders[PAIR]
             .filter(o => o.status == "NEW" || o.status == "PARTIALLY_FILLED")

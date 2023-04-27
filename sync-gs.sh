4add90e5676a4f1669f963a98377c82d12daf423
Elihai Vanunu <elihaidv@gmail.com>
2023-04-27 10:46:31 +0300

    A

--- sync-gs.sh
+++ sync-gs.sh
@@ -8,6 +8,6 @@
     
 mv simulations-outputs/* simulations-outputs-back
 for f in simulations-outputs-back/simulation*;
- do /home/clap/Downloads/google-cloud-sdk/bin/gsutil -m rsync -r -i $f gs://simulations-tradingbot/$(basename "$f"); 
+ do gsutil -m rsync -r -i $f gs://simulations-tradingbot/$(basename "$f"); 
  rm -rf $f
 done;

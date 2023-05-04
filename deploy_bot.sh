a49e34de0feb155ab348dc314dde6ec0984c3ede
Elihai Vanunu <elihaidv@gmail.com>
2023-05-04 09:50:20 +0300

    A

--- deploy_bot.sh
+++ deploy_bot.sh
@@ -1,5 +1,5 @@
 tsc --outDir build -p tsconfig.json 
-cp package.json trading-cloud.json utils build
+cp -r package.json trading-cloud.json utils build
 
 rsync -rzvPu  -r --exclude build/DB.js build/* root@trading:/var/www/bot
 ssh root@trading  "/root/.nvm/versions/node/v17.9.1/bin/pm2 reload Main"

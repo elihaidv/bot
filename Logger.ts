324f51f83c60d5a6ba4672cbfeedc37e66689e69
Lotem Vanunu <lotem@closeapp.co.il>
2024-09-10 19:00:41 +0300

    A

--- Logger.ts
+++ Logger.ts
@@ -1,6 +1,7 @@
 import { CoralogixLogger, Log, LoggerConfig, Severity } from "coralogix-logger";
 import { env } from "node:process";
 import DB from "./DB.js"
+import {promises as fs} from 'fs'
 
 export class BotLogger {
     static instance: BotLogger = new BotLogger();
@@ -12,18 +13,46 @@ export class BotLogger {
 
     logger: CoralogixLogger = new CoralogixLogger("Bot")
 
+    localLogsPath = "logs/"
+    localLogs: {[key: string]:any[]} = {}
+
     constructor() {
+        this.initLocalLogs();
         if (!this.isSemulation && DB.ENVIROMENT != "LOCAL") {
             CoralogixLogger.configure(this.config);
         }
     }
 
+    async initLocalLogs() {
+        if (await fs.stat(this.localLogsPath).catch(e => null) == null) {
+            await fs.mkdir(this.localLogsPath)
+        }
+        const files = await fs.readdir(this.localLogsPath)
+        files.forEach(async file => {
+            const content = await fs.readFile(this.localLogsPath + file)
+            this.localLogs[file] = JSON.parse(content.toString())
+        })
+    }
+
+    saveLogFile(fileName: string) {
+        fs.writeFile(this.localLogsPath + fileName, JSON.stringify(this.localLogs[fileName]))
+    }
+
     log(message: any, severity: Severity = Severity.info) {
         if (!this.isSemulation && DB.ENVIROMENT != "LOCAL") {
-            this.logger.addLog(new Log({
+            const logKey = (new Date().toISOString().split(":")[0]) + "~" + (message.bot_id || "general")
+            
+            this.localLogs[logKey] ||= []
+
+            const logEntry = new Log({
                 severity: severity,
                 text: JSON.stringify(message, (k, v)=> v === undefined ? null : v)
-            }));
+            })
+
+            this.localLogs[logKey].push(logEntry)
+            this.saveLogFile(logKey)
+            
+            this.logger.addLog(logEntry);
         }
     }
 

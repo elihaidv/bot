710f1440e9bc2a392ec1767f0a89a00504c1e60f
Lotem Vanunu <lotem@closeapp.co.il>
2024-09-10 19:27:41 +0300

    A

--- Logger.ts
+++ Logger.ts
@@ -1,67 +1,75 @@
 import { CoralogixLogger, Log, LoggerConfig, Severity } from "coralogix-logger";
 import { env } from "node:process";
-import DB from "./DB.js"
-import {promises as fs} from 'fs'
+import DB from "./DB.js";
+import { promises as fs } from "fs";
 
 export class BotLogger {
-    static instance: BotLogger = new BotLogger();
-    config = new LoggerConfig({
-        applicationName: "itamarbot",
-        privateKey: "7ac54874-4e53-5903-9122-8db714a125dc",
-        subsystemName: "production",
+  static instance: BotLogger = new BotLogger();
+  config = new LoggerConfig({
+    applicationName: "itamarbot",
+    privateKey: "7ac54874-4e53-5903-9122-8db714a125dc",
+    subsystemName: "production",
+  });
+
+  logger: CoralogixLogger = new CoralogixLogger("Bot");
+
+  localLogsPath = "logs/";
+  localLogs: { [key: string]: any[] } = {};
+
+  constructor() {
+    this.initLocalLogs();
+    if (!this.isSemulation && DB.ENVIROMENT != "LOCAL") {
+      CoralogixLogger.configure(this.config);
+    }
+  }
+
+  async initLocalLogs() {
+    if ((await fs.stat(this.localLogsPath).catch((e) => null)) == null) {
+      await fs.mkdir(this.localLogsPath);
+    }
+    const files = await fs.readdir(this.localLogsPath);
+    files.forEach(async (file) => {
+      const content = await fs.readFile(this.localLogsPath + file);
+      this.localLogs[file] = JSON.parse(content.toString());
     });
+  }
 
-    logger: CoralogixLogger = new CoralogixLogger("Bot")
+  saveLogFile(fileName: string) {
+    fs.writeFile(
+      this.localLogsPath + fileName,
+      JSON.stringify(this.localLogs[fileName])
+    );
+  }
 
-    localLogsPath = "logs/"
-    localLogs: {[key: string]:any[]} = {}
+  log(message: any, severity: Severity = Severity.info) {
+    if (!this.isSemulation && DB.ENVIROMENT != "LOCAL") {
+      const logKey =
+        new Date().toISOString().split(":")[0] +
+        "~" +
+        (message.bot_id || "general");
 
-    constructor() {
-        this.initLocalLogs();
-        if (!this.isSemulation && DB.ENVIROMENT != "LOCAL") {
-            CoralogixLogger.configure(this.config);
-        }
-    }
+      this.localLogs[logKey] ||= [];
 
-    async initLocalLogs() {
-        if (await fs.stat(this.localLogsPath).catch(e => null) == null) {
-            await fs.mkdir(this.localLogsPath)
-        }
-        const files = await fs.readdir(this.localLogsPath)
-        files.forEach(async file => {
-            const content = await fs.readFile(this.localLogsPath + file)
-            this.localLogs[file] = JSON.parse(content.toString())
+      this.localLogs[logKey].push({
+        severity: severity,
+        text: message,
+      });
+      this.saveLogFile(logKey);
+
+      this.logger.addLog(
+        new Log({
+          severity: severity,
+          text: JSON.stringify(message, (k, v) => (v === undefined ? null : v)),
         })
+      );
     }
+  }
 
-    saveLogFile(fileName: string) {
-        fs.writeFile(this.localLogsPath + fileName, JSON.stringify(this.localLogs[fileName]))
-    }
+  error(message: any) {
+    this.log(message, Severity.error);
+  }
 
-    log(message: any, severity: Severity = Severity.info) {
-        if (!this.isSemulation && DB.ENVIROMENT != "LOCAL") {
-            const logKey = (new Date().toISOString().split(":")[0]) + "~" + (message.bot_id || "general")
-            
-            this.localLogs[logKey] ||= []
-
-            const logEntry = new Log({
-                severity: severity,
-                text: JSON.stringify(message, (k, v)=> v === undefined ? null : v)
-            })
-
-            this.localLogs[logKey].push(logEntry)
-            this.saveLogFile(logKey)
-            
-            this.logger.addLog(logEntry);
-        }
-    }
-
-    error(message: any) {
-        this.log(message, Severity.error)
-    }
-
-    get isSemulation() {
-        
-        return env.IS_SIMULATION == "true"
-    }
+  get isSemulation() {
+    return env.IS_SIMULATION == "true";
+  }
 }
--- utils/uploadDay.js
+++ utils/uploadDay.js
@@ -17,36 +17,36 @@ function timeout(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-// fs.readdir(`future`, (err, dirs) => {
-//     for (let d of dirs) {
-//         const SYMBOL = d.split('.')[0]
-//         fs.readdir(`future/${d}`, (err, files) => {
-//             for (let f of files) {
-//                 const date = f.split(".")[0]
-//                 fs.readFile(`future/${d}/${f}`, 'utf8', (err, data) => {
-//                     if (err) {
-//                         console.error(err)
-//                         return
-//                     }
-//                     const lines = data.split("\n")
-//                         .map(l => l.split(","))
-//                         .map(l => [(parseInt(l[0] / 1000)) * 1000, l[1], l[1], l[1]])
+fs.readdir(`future`, (err, dirs) => {
+    for (let d of dirs) {
+        const SYMBOL = d.split('.')[0]
+        fs.readdir(`future/${d}`, (err, files) => {
+            for (let f of files) {
+                const date = f.split(".")[0]
+                fs.readFile(`future/${d}/${f}`, 'utf8', (err, data) => {
+                    if (err) {
+                        console.error(err)
+                        return
+                    }
+                    const lines = data.split("\n")
+                        .map(l => l.split(","))
+                        .map(l => [(parseInt(l[0] / 1000)) * 1000, l[1], l[1], l[1]])
 
 
-//                     new Storage()
-//                         .bucket('crypto-history')
-//                         .file(`futures/${SYMBOL}/1s/${date}.csv`)
-//                         .save(lines.map(l => l.join(',')).join('\n'), { resumable: false })
-//                         .then(console.log)
-//                         .catch(console.log);
-//                 })
-//                 if (new Date().getTime() - new Date(date).getTime() > SECONDS_IN_DAY) {
-//                     exec(`rm future/${d}/${f}`)
-//                 }
-//             }
-//         })
-//     }
-// })
+                    new Storage()
+                        .bucket('crypto-history')
+                        .file(`futures/${SYMBOL}/1s/${date}.csv`)
+                        .save(lines.map(l => l.join(',')).join('\n'), { resumable: false })
+                        .then(console.log)
+                        .catch(console.log);
+                })
+                if (new Date().getTime() - new Date(date).getTime() > SECONDS_IN_DAY) {
+                    exec(`rm future/${d}/${f}`)
+                }
+            }
+        })
+    }
+})
 
 fs.readdir(`logs`, (err, files) => {
     for (let f of files) {
